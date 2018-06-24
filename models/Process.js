fc5880120509ee2b09ab5ec5686ec14e095dc0a3
Menighin <joao.menighin@gmail.com>
2018-06-24 18:05:54 -0300

    Progress ba step - In Progress

--- models/Process.js
+++ models/Process.js
@@ -59,8 +59,8 @@ class Process {
     set progress(progress) { 
         let brackets = progress.match(/\[(.*?)\]/)[1].split('/');
         this._progress = {
-            percent: (parseInt(brackets[0] - 1)) / parseInt(brackets[1]),
-            nextPercent: (parseInt(brackets[0])) / parseInt(brackets[1]),
+            step: parseInt(brackets[0]),
+            totalSteps: parseInt(brackets[1]),
             message: progress.split(']')[1]
         };
     }
