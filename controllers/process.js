a0db028a97a2d93308bc1d96e2289a099bb950ad
Menighin <joao.menighin@gmail.com>
2018-07-03 20:22:37 -0300

    Downloading input

--- controllers/process.js
+++ controllers/process.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import LogService from '../services/LogService';
+import QueueConfigurations from '../utils/QueueConfigurations';
 
 const router = express.Router();
 
 router.get('/downloadLog', (req, res) => {
-    var file = LogService.getLogFilename(req.query.id, req.query.name);
+    let file = LogService.getLogFilename(req.query.id, req.query.name);
+    res.download(file);
+});
+
+router.get('/downloadInput', (req, res) => {
+    let file = `${QueueConfigurations.get('exe_directory')}\\${req.query.id}_input`;
     res.download(file);
 });
 
